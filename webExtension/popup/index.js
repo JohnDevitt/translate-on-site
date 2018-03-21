cd6e522b175485dce0910ec597322f2068082068
John Devitt <johndevittire@gmail.com>
2018-03-21 18:34:05 +0100

    UI started

--- webExtension/popup/index.js
+++ webExtension/popup/index.js
@@ -1,6 +1,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    options: [{
+        value: 'English',
+        label: 'English'
+      }, {
+        value: 'German',
+        label: 'German'
+      }, {
+        value: 'French',
+        label: 'French'
+      }, {
+        value: 'Japanese',
+        label: 'Japanese'
+      }, {
+        value: 'Russian',
+        label: 'Russian'
+      }],
+    value: 'French',
+    checked: true
   }
 })
