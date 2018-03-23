a036d3664e656c07b3d4f1ded1f49098599c6f66
John Devitt <johndevittire@gmail.com>
2018-03-23 17:24:46 +0100

    updates to config files

--- src/content_script/translateDocument.js
+++ src/content_script/translateDocument.js
@@ -1,44 +1,46 @@
-import _ from 'lodash'
-import getVisibleText from './getVisibleText'
+import _ from "lodash";
+import getVisibleText from "./getVisibleText";
 
 const walkDOM = (element, setTranslationID, DOMNodesWithVisibleText) => {
-  const visibleText = getVisibleText(element)
-  const trimmedText = _.trim(visibleText)
+  const visibleText = getVisibleText(element);
+  const trimmedText = _.trim(visibleText);
   if (visibleText && trimmedText) {
-    const translationID = setTranslationID(element)
-    DOMNodesWithVisibleText.push({id: translationID, text: visibleText})
+    const translationID = setTranslationID(element);
+    DOMNodesWithVisibleText.push({ id: translationID, text: visibleText });
   }
   if (element.children.length > 0) {
     Array.from(element.children).forEach(child =>
       walkDOM(child, setTranslationID, DOMNodesWithVisibleText)
-    )
+    );
   }
-}
+};
 
 (async () => {
-  let count = 0
+  let count = 0;
   const setTranslationID = element => {
-    const translationID = 'translateOnSiteInsertedID' + count++
-    element.setAttribute('translationID', translationID)
-    return translationID
-  }
-  let DOMNodesWithVisibleText = []
-  walkDOM(document.body, setTranslationID, DOMNodesWithVisibleText)
+    const translationID = "translateOnSiteInsertedID" + count++;
+    element.setAttribute("translationID", translationID);
+    return translationID;
+  };
+  let DOMNodesWithVisibleText = [];
+  walkDOM(document.body, setTranslationID, DOMNodesWithVisibleText);
 
-  const visibleTextList = DOMNodesWithVisibleText.map(element => element.text)
-  const elementsWithVisibleText = DOMNodesWithVisibleText.map(element => element.id)
+  const visibleTextList = DOMNodesWithVisibleText.map(element => element.text);
+  const elementsWithVisibleText = DOMNodesWithVisibleText.map(
+    element => element.id
+  );
 
   let reqObject = {
-    method: 'POST',
+    method: "POST",
     headers: new Headers({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     }),
     body: JSON.stringify({
-      target: 'es',
+      target: "es",
       visibleTextList
     })
-  }
-/*
+  };
+  /*
   const res = await fetch('http://localhost:3000/translate', reqObject)
   const translatedList = await res.json()
 
@@ -46,4 +48,4 @@ const walkDOM = (element, setTranslationID, DOMNodesWithVisibleText) => {
     document.querySelector('[translationID=' + element[0] + ']').childNodes[0].nodeValue = element[1]
   })
 */
-})()
+})();
